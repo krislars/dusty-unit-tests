3addffca320628abdef03f0781481800bc74edf3
Kristen Larson <krislars@gmail.com>
2024-04-14 22:20:34 -0600

    Update add_two_numbers.py
    Change so both tests pass

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -8,4 +8,4 @@ def test_add_negatives():
 
 def test_add_positives():
     result = add_two_numbers(1, 3)
-    assert result == -4, 'Test add positives failed'
+    assert result == 4, 'Test add positives failed'
